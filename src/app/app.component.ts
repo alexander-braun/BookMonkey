0e2cee0fe5f67f428dad0cbd5d509bff9f1aa9c8
alexander-braun <alexanderbraun@freenet.de>
2020-10-20 22:45:55 +0200

    add interfaces for book and book-thumbnail

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'bm-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'BookMonkey';
+  name = { peo: null };
+  myUrl = '';
+  hasError = true;
+  angularVersion = 1;
+  names = ['name 1', 'name 2', 'name 3', 'name x'];
+  myClickhandler(): void {
+    console.log('Click');
+  }
 }
